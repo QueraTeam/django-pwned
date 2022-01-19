b79602e229ebc7dc57ca24918ffd05fe0f6951fb
Mohammad Javad Naderi <mjnaderi@gmail.com>
2022-01-19 09:15:40 +0330

    Add pre-commit

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 import os
+
+from setuptools import find_packages, setup
+
 from django_pwned import __version__
-from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), "README.md"), "r", encoding="UTF-8") as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+dev_requirements = ["pre-commit"]
 
 setup(
     name="django-pwned",
@@ -17,16 +21,19 @@ setup(
     author="Mohammad Javad Naderi",
     packages=find_packages(".", include=("django_pwned", "django_pwned.*")),
     include_package_data=True,
-    install_requires=["Django>=3.1", "requests"],
+    install_requires=["Django>=3.2", "requests"],
+    extras_require={"dev": dev_requirements},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Internet :: WWW/HTTP :: Session",
         "Topic :: Security",
     ],
