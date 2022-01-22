1a7f4f50fa686d4e10f7a1c4e648480e34fdde25
Mohammad Javad Naderi <mjnaderi@gmail.com>
2022-01-22 10:11:34 +0330

    Add url to setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     author="Mohammad Javad Naderi",
+    url="https://github.com/QueraTeam/django-pwned",
+    download_url="https://pypi.org/project/django-pwned/",
     packages=find_packages(".", include=("django_pwned", "django_pwned.*")),
     include_package_data=True,
     install_requires=["Django>=3.2", "requests"],
